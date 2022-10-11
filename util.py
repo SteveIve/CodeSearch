451deafbb6f43111c2f7383fe4cba393ad1177fc
Pengcheng Liu <lpc@cqu.edu.cn>
2022-10-11 16:26:40 +0800

    implemented the xml2csv function. tired today. miss my sophia so much.

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def filter_closed(path_in, path_out):
     with open(path_in, "r", encoding="utf-8") as f_in:
         with open(path_out, "w", encoding="utf-8") as f_out:
@@ -7,17 +10,31 @@ def filter_closed(path_in, path_out):
                     f_out.write(line)
 
 
-def xml2csv(file):
-    # TODO
-    pass
+def xml2csv(read_path, write_path):
+    """
+    convert xml into csv, All attributes but question title will be removed.
+
+    :param read_path: i.e. path of xml file
+    :param write_path: i.e. path of csv file
+    """
+    import xml.etree.ElementTree as ET
+    with open(read_path, 'r', encoding='latin1') as f_read:
+        read_lines = f_read.readlines()
+        line_num = len(read_lines)
+        with open(write_path, 'w', encoding='utf-8', newline='') as f_write:
+            csv_writer = csv.writer(f_write)
+            keys = list(dict(ET.fromstring(read_lines[0]).attrib).keys())
+            for i in range(line_num):
+                line = read_lines[i]
+                post = dict(ET.fromstring(line).attrib)
+                csv_writer.writerow([i, post['Body']])
+                f_write.flush()
+
+
+
+
 
 
 def csv_split(backups: int):
     # TODO
     pass
-
-
-def keep_questions_only(csv_file):
-    # TODO
-    pass
-
