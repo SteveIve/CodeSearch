3400842102b826b8a0af6e3d3ccfadb6e492f78a
Pengcheng Liu <lpc@cqu.edu.cn>
2022-10-13 16:50:13 +0800

    created main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import zipfile
+
+import util
+
+# First, we unzip the posts_java.zip file
+with zipfile.ZipFile('raw-data/posts_java.zip') as zf:
+    zf.extractall('raw-data/')
+
+# Filter out posts with negative score
+util.filter_negative_score("raw-data/posts_java.xml", "raw-data/posts_without_neg.xml")
+print("done filtering neg")
+
+# Filter out posts with closed tag
+util.filter_closed('raw-data/posts_without_neg.xml', 'raw-data/posts_without_closed.xml')
+print("done filtering closed")
+
+# Convert xml into csv and keep questions only
+util.xml2csv('raw-data/posts_without_closed.xml', 'raw-data/posts.csv', True)
+print("done converting into csv")
+
+# Split
+util.csv_split('raw-data/posts.csv', 'split/', 10000)
+print("done spliting")
